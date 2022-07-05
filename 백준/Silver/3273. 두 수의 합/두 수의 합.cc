a88f4d9722b80b6f637820b54064233a90994eb0
Chang-Min Lee <92534037+changchang4825@users.noreply.github.com>
2022-07-05 17:30:04 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 3388 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -16,11 +16,11 @@ int main() {
 
     cin >> x;
     for (int i = 0; i < n; i++) {
-        if (x - idx[i] <= 0 || x - idx[i] > 1000000 || x - idx[i] == idx[i] || !arr[idx[i]] || !arr[x - idx[i]]) continue;
+        if (x <= idx[i] || x - idx[i] > 1000000 || x - idx[i] == idx[i] || !arr[idx[i]] || !arr[x - idx[i]]) continue;
         arr[idx[i]] = 0;
         ans++;
     }
     cout << ans;
 
     return 0;
-}   
+}
