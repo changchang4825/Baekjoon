f0c6ca831997763a8018be1201d837ad042a0d8e
Chang-Min Lee <92534037+changchang4825@users.noreply.github.com>
2022-07-05 16:07:30 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+string S;
+int alphabet[26];
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string S;
-    int a[26] = { 0 };
     cin >> S;
-    
-    for (int i = 0; i < S.length(); i++) a[S[i] - 'a']++;
-    for (int i = 0; i < 26; i++) cout << a[i] << " ";
+    for (char i : S) alphabet[i - 'a']++;
+    for (int i : alphabet) cout << i << " ";
 
     return 0;
 }
